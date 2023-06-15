82739e829d638eb03ff2111b046b4c1268fd0a6d
Alex Kuzin <acidkarma3000@gmail.com>
2023-06-15 15:43:56 +0500

    addWork6

---
+++ src/main/java/Service/PosterService.java
@@ -0,0 +1,47 @@
+package Service;
+
+
+import Domain.Poster;
+
+public class PosterService {
+    private int limit;
+
+    public PosterService() {
+        this.limit = 5;
+    }
+
+    public PosterService(int limit) {
+        this.limit = limit;
+    }
+
+    private Poster[] poster = new Poster[0];
+
+    public void addPoster(Poster item) {
+        Poster[] tmp = new Poster[poster.length + 1];
+        for (int i = 0; i < poster.length; i++) {
+            tmp[i] = poster[i];
+        }
+        tmp[tmp.length - 1] = item;
+        poster = tmp;
+    }
+
+    public Poster[] getItems() {
+
+        return poster;
+    }
+
+    public Poster[] findLast() {
+        int resultLength;
+        if (poster.length < limit) {
+            resultLength = poster.length;
+        } else {
+            resultLength = limit;
+        }
+        Poster[] tmp = new Poster[resultLength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = poster[poster.length - 1 - i];
+        }
+        return tmp;
+    }
+
+}
